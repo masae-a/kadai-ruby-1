53082a6b9f2a9340e6f61f3d99c4a750c869e0cf
masae-a <mse.spwi@gmail.com>
2023-01-15 08:12:28 +0000

    kadai-introduce.rb

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,6 +1,4 @@
-name = '青木'
-name1 = '昌栄'
-str = 'です。'
-age = 33
-str = '歳です。'
-puts puts name + name1 + str + age.to_s + str
+myouji = '青木'
+namae = '昌栄'
+age = 34
+puts myouji + namae + "です。" + age.to_s + "歳です。"
